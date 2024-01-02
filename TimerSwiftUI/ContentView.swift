73f1795d6086a0830060ef3fca9700f7418caf4e
i-qbu <iqbu.lab@gmail.com>
2024-01-02 21:36:10 +0900

    Initial Commit

---
+++ TimerSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimerSwiftUI
+//
+//  Created by 渡辺大智 on 2024/01/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimerSwiftUI/TimerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerSwiftUIApp.swift
+//  TimerSwiftUI
+//
+//  Created by 渡辺大智 on 2024/01/02.
+//
+
+import SwiftUI
+
+@main
+struct TimerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
